63cb4046e2bddd8863bd3a0d0e65491cb031d6fd
Z2015212113 <472949559@qq.com>
2019-03-28 18:26:01 +0800

    optimised code

--- apps/MarkManagement/view/TeacherManageView.py
+++ apps/MarkManagement/view/TeacherManageView.py
@@ -27,13 +27,16 @@ class TeacherManageViewSet(viewsets.ViewSet):
                     查询成功，返回JSON response包括code, message, subjects, count，状态码2000
         """
         access_token = request.META.get("HTTP_TOKEN")
-
         if not token_verify(access_token):
             return token_invalid()
+
         teacher_set = Teacher.objects.filter(token__token_text=access_token)
+
         teacher = teacher_set[0]
+
         if not teacher.is_manager:
             return manager_check_failed()
+
         id = request.GET.get('id')
         tid = request.GET.get('tid')
         name = request.GET.get('name')
@@ -41,8 +44,11 @@ class TeacherManageViewSet(viewsets.ViewSet):
         email = request.GET.get('email')
         mobile = request.GET.get('mobile')
         is_manager = request.GET.get('is_manager')
-        if id is None and tid is None and name is None and college_id is None and email is None and mobile is None  and is_manager is None:
+
+        if id is None and tid is None and name is None and college_id is None \
+                and email is None and mobile is None and is_manager is None:
             return parameter_missed()
+
         teacher_set = Teacher.objects.all()
         if id is not None:
             teacher_set = teacher_set.filter(id=id)
@@ -61,10 +67,13 @@ class TeacherManageViewSet(viewsets.ViewSet):
 
         teacher_set = teacher_set.values()
         result = []
+
         for teacher in teacher_set:
             result.append(teacher)
+
         if len(result) == 0:
             return query_failed()
+
         code_number = '2000'
         result = {
             'code': code_number,
@@ -86,21 +95,23 @@ class TeacherManageViewSet(viewsets.ViewSet):
                     插入失败，返回insert_failed的JSON response
                     插入成功，返回JSON response包括code, message, subjects，状态码2001
         """
-        post_data = request.data
         access_token = request.META.get("HTTP_TOKEN")
         if not token_verify(access_token):
             return token_invalid()
+
         teacher_set = Teacher.objects.filter(token__token_text=access_token)
         teacher = teacher_set[0]
         if not teacher.is_manager:
             return manager_check_failed()
 
+        post_data = request.data
         subjects = post_data.get('subjects')
         if subjects is None:
             return parameter_missed()
 
         tag = False
         ids = []
+
         for subjectDict in subjects:
             tid = subjectDict.get('tid')
             name = subjectDict.get('name')
@@ -108,8 +119,10 @@ class TeacherManageViewSet(viewsets.ViewSet):
             email = subjectDict.get('email')
             mobile = subjectDict.get('mobile')
             password = subjectDict.get('password')
+
             if tid is None or name is None or college_id is None:
                 continue
+
             teacher = Teacher()
             if tid:
                 teacher.tid = tid
@@ -117,8 +130,10 @@ class TeacherManageViewSet(viewsets.ViewSet):
                 teacher.name = name
             if college_id:
                 college_set = College.objects.filter(id=college_id)
+
                 if college_set.count() == 0:
                     continue
+
                 teacher.college = college_set[0]
             if email:
                 teacher.email = email
@@ -126,16 +141,21 @@ class TeacherManageViewSet(viewsets.ViewSet):
                 teacher.mobile = mobile
             if password:
                 teacher.password = password
+
             try:
                 teacher.save()
             except Exception as e:
                 continue
+
             else:
-                ids.append({'id':teacher.id})
+                ids.append({'id': teacher.id})
                 tag = True
 
         if tag:
-            return JsonResponse({'subjects':ids, 'code': '2001', 'message': status_code['2001']}, safe=False)
+            return JsonResponse(
+                {'subjects': ids,
+                 'code': '2001',
+                 'message': status_code['2001']}, safe=False)
         else:
             return insert_failed()
 
@@ -150,20 +170,24 @@ class TeacherManageViewSet(viewsets.ViewSet):
                     更新失败，返回update_failed的JSON response
                     更新成功，返回JSON reponse包括code, message, subjects，状态码2005
         """
-        put_data =  request.data
         access_token = request.META.get("HTTP_TOKEN")
         if not token_verify(access_token):
             return token_invalid()
+
         teacher_set = Teacher.objects.filter(token__token_text=access_token)
         teacher = teacher_set[0]
         if not teacher.is_manager:
             return manager_check_failed()
 
+        put_data = request.data
         subjects = put_data.get('subjects')
+
         if subjects is None:
             return parameter_missed()
+
         tag = False
         ids = []
+
         for subjectDict in subjects:
             id = subjectDict.get('id')
             tid = subjectDict.get('tid')
@@ -172,6 +196,7 @@ class TeacherManageViewSet(viewsets.ViewSet):
             mobile = subjectDict.get('mobile')
             email = subjectDict.get('email')
             teacher_set = Teacher.objects.filter(id=id)
+
             for teacher in teacher_set:
                 if tid:
                     teacher.tid = tid
@@ -183,14 +208,21 @@ class TeacherManageViewSet(viewsets.ViewSet):
                     teacher.name = name
                 if college_id:
                     college_set = College.objects.filter(id=college_id)
+
                     if not college_set.exists():
                         continue
+
                     teacher.college = college_set[0]
+
                 teacher.save()
                 ids.append({'id':teacher.id})
                 tag = True
+
         if tag:
-            return JsonResponse({'subjects':ids, 'code': '2005', 'message': status_code['2005']}, safe=False)
+            return JsonResponse(
+                {'subjects': ids,
+                 'code': '2005',
+                 'message': status_code['2005']}, safe=False)
         else:
             return update_failed()
 
@@ -210,6 +242,7 @@ class TeacherManageViewSet(viewsets.ViewSet):
 
         if not token_verify(access_token):
             return token_invalid()
+
         teacher_set = Teacher.objects.filter(token__token_text=access_token)
         teacher = teacher_set[0]
         if not teacher.is_manager:
@@ -218,20 +251,26 @@ class TeacherManageViewSet(viewsets.ViewSet):
         subjects = delete_data.get('subjects')
         if subjects is None:
             return parameter_missed()
+
         tag = False
+
         for subjectDict in subjects:
             id = subjectDict.get('id')
+
             if id is None:
                 continue
+
             teacher_set = Teacher.objects.filter(id=id)
+
             if not teacher_set.exists():
                 continue
+
             try:
                 teacher_set.delete()
+                tag = True
             except Exception as e:
                 continue
-            else:
-                tag = True
+
         if tag:
             return delete_succeed()
         else:
